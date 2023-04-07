f56b501a1cfb199e8e8d1b6c558e1727bb638382
wellerson <wellersonjava@gmail.com>
2023-04-06 22:02:55 -0300

    Remoção de pastas antigas e adição do teste

--- Desafior-Iniciante/ExerciciosBee/src/classes/AreaCircTeste.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class AreaCircTeste {
-    public static void main(String[] args) {
-    AreaCirq a = new AreaCirq(5.6);
-
-        System.out.println(a.area());
-    }
-}
--- Desafior-Iniciante/ExerciciosBee/src/classes/ValorNulo.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class ValorNulo {
-    public static void main(String[] args){
-        String s1 = "";
-        System.out.println(s1.concat("!!!!"));
-    }
-}
